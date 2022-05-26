ceaff9db338678c90d24f16cf72dd39130a3655a
Matt Crecraft <mcrecraft@gmail.com>
2022-05-26 11:32:54 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const ask = require("./ask")
 
-const askQuestion = (question) => {
-  ask(question).then(result => {
+// const askQuestion = (question) => {
+//   ask(question).then(result => {
+//     console.log('Promise 1:', result.response)
+//     .ask(question).then(result => {
+//       console.log('Promise 2:', result.response)
+//     })
+//   })
+// }
+
+// askQuestion("Why is the sky blue?")
+
+async function askQuestion(question) {
+  await ask(question).then(result => {
     console.log('Promise 1:', result.response)
-    .ask(question).then(result => {
+  }).ask(question).then(result => {
       console.log('Promise 2:', result.response)
-    })
   })
 }
 
-askQuestion("Why is the sky blue?")
+ askQuestion("Why is the sky blue?")
